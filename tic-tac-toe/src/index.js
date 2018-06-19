53f55cec1ed01a34af8fb764dd2a00f67a6e6dcc
Alan Cugler <alan.cugler101@gmail.com>
2018-06-19 14:39:23 -0500

    Created a new variable in Game's render function called 'moves'. A button is been assigned to this to allow reverting to previous move.

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -75,6 +75,17 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calcWin(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={ () => this.jumoTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+    
     let status;
     if (winner){
       status = 'Winner: ' + (this.state.xIsNext ? 'X' : 'O');
